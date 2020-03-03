fc411954fd3f56c2a80bf7f75d3b885043a964ac
roberto araya gil <naggas@Naggas.local>
2020-03-03 13:45:27 +0100

    themes created by frontal

--- src/components/user/Admin/Topics.jsx
+++ src/components/user/Admin/Topics.jsx
@@ -2,10 +2,45 @@ import React, { Component } from "react";
 import TopicSevices from "../../../services/topics.sevices";
 import TopicDetail from "./TopicDetail";
 import "../../../styles/Admin/Topics.css";
+import topicsSevices from "../../../services/topics.sevices";
 
 class Topics extends Component {
   state = {
-    topics: []
+    topics: [],
+    newTopic: {
+      name: ""
+    },
+    success: false,
+    error: false
+  };
+
+  handelChange = event => {
+    const { name, value, files } = event.target;
+
+    this.setState({
+      newTopic: {
+        ...this.state.newTopic,
+        [name]: files ? files[0] : value
+      }
+    });
+  };
+
+  handelSubmit = event => {
+    event.preventDefault();
+
+    const { newTopic } = this.state;
+
+    this.setState({ error: false }, () => {
+      topicsSevices
+        .newTopic(newTopic)
+        .then(() => {
+          window.location.reload();
+          this.setState({ success: true });
+        })
+        .catch(() => {
+          this.setState({ error: true });
+        });
+    });
   };
 
   componentDidMount() {
@@ -22,27 +57,25 @@ class Topics extends Component {
         <div className="create-topic">
           <button
             type="button"
-            className="btn btn-primary"
+            className="btn btn-success"
             data-toggle="modal"
-            data-target="#exampleModal"
+            data-target="#newForm"
           >
-            Launch demo modal
+            Nuevo tema
           </button>
 
           <div
             className="modal fade"
-            id="exampleModal"
+            id="newForm"
             tabIndex="-1"
             role="dialog"
             aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
+            aria-hidden="false"
           >
             <div className="modal-dialog" role="document">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel">
-                    Modal title
-                  </h5>
+                  <h5 className="modal-title">Nuevo Tema</h5>
                   <button
                     type="button"
                     className="close"
@@ -52,18 +85,25 @@ class Topics extends Component {
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
-                <div className="modal-body">...</div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
+                <div className="modal-body">
+                  <form className="container" onSubmit={this.handelSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="name">Nombre del tema</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        value={this.state.newTopic.name}
+                        onChange={this.handelChange}
+                        autoComplete="off"
+                      />
+                    </div>
+                    <div className="modal-footer">
+                      <button type="submit" className="btn btn-success">
+                        Crear
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
